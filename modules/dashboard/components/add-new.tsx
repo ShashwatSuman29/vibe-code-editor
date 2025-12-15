fb5283862b58dbc216a468a0fea14cc6da5b8d69
Shashwat Suman <shashwatsumanat29@gmail.com>
2025-12-15 18:13:41 +0530

    template screen modal Ui

--- modules/dashboard/components/add-new.tsx
+++ modules/dashboard/components/add-new.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
+import TemplateSelectingModel from "./template-selecting-modal";
 
 const AddNewButton = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -51,6 +52,11 @@ const AddNewButton = () => {
         </div>
       </div>
       {/* Todo Implement Template Selecting Model here */}
+      <TemplateSelectingModel
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onSubmit={() => {}}
+      />
     </>
   );
 };
